d90825802b26e32d45a173edda76ddbfd394d0dc
Dimpal Dhage <dimpaldhage1@gmail.com>
2024-10-10 22:58:27 +0530

    Recaptcha

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function beforesubmit(){
+function beforesubmit() {
+  let inputdate = document.querySelector(".inputdate");
+  let outputdate = document.querySelector(".outputdate");
+  console.log("input value=", inputdate.value);
 
-    let inputdate= document.querySelector(".inputdate");
-    let outputdate = document.querySelector(".outputdate");
-    console.log("input value=",inputdate.value);
-
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); 
-    outputdate.value= formattedDate;
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formattedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
