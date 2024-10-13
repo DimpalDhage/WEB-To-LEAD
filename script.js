b14491031fb0cb507f47def88069d6f69ebfe554
Dimpal Dhage <dimpaldhage1@gmail.com>
2024-10-13 18:17:40 +0530

    Bug fix

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function beforesubmit() {
-  let inputdate = document.querySelector(".inputdate");
-  let outputdate = document.querySelector(".outputdate");
-  console.log("input value=", inputdate.value);
+let captchachecked ="false";
+if(captchachecked){
 
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formattedDate;
+  function beforesubmit(event) {
+
+      let inputdate = document.querySelector(".inputdate");
+      let outputdate = document.querySelector(".outputdate");
+      console.log("input value=", inputdate.value);
+
+      let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+      outputdate.value = formattedDate;
+  }
 }
+else{
 
+  alert("please check recaptcha before submit the form");
+  event.preventDefault();//to stop form submitting if we don't click on captcha.
+}
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
   if (response == null || response.value.trim() == "") {
@@ -19,3 +28,8 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchasuccess(){
+
+  captchachecked ="true";
+}
